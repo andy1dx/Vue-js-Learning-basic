c2dc0017277f6dfed081e24a7466924ba77b9936
andy william <andy.haz3@google.com>
2018-07-18 16:55:06 +0900

    Setting up first component controller VUE

---
+++ app.js
@@ -0,0 +1,8 @@
+//Making new vue conmponent controller
+new Vue({
+    el: '#vue-app', //container that will be control by this vue
+    // data that set by vue will only effect inside the ID container
+    data: {
+        name: 'Shaun'
+    }
+});
